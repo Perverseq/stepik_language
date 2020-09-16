be2afc55ec05b518aa8fd48b5aabb8363b42afa2
Perverseq <luchniknya@gmail.com>
2020-09-16 21:18:21 +0500

    updated assert

--- test_items.py
+++ test_items.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -10,6 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
     # time.sleep(10)
-    assert WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//form[@id="add_to_basket_form"]'
-                                                  '//button[@type="submit"]'))), 'Кнопка не найдена.'
+    assert browser.find_element_by_xpath('//form[@id="add_to_basket_form"]//button[@type="submit"]'), \
+        'Кнопка не найдена'
